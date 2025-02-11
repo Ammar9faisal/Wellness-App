ba9c19b9f0788927dc16cd681a15eee70f8fb932
Ammar9faisal <134812365+Ammar9faisal@users.noreply.github.com>
2025-02-11 13:34:58 -0500

    Initialized Appwrite for backend services such as user authentication and database

---
+++ src/appwrite.js
@@ -0,0 +1,8 @@
+import { Client, Account, ID} from "appwrite";
+
+const client = new Client();
+client.setProject('67aab3000007ccf67076');
+
+const account = new Account(client);
+
+export { client, account };
