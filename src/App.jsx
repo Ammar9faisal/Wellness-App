6cd438e9c28369467a23eefbd98e583ef59d3e62
Ammar9faisal <134812365+Ammar9faisal@users.noreply.github.com>
2025-02-11 23:43:17 -0500

    Created a functional login screen with password and Outh2 Google signin

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,94 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import React from 'react';
+import { account } from './appwrite';
+import GoogleButton from 'react-google-button'
+import { useState } from 'react';
+import background from './assets/Purple.png';
+
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [name, setName] = useState(""); //initialized the state of inputs for sign up
+  const [email, setUser] = useState("");
+  const [password, setPassword] = useState("");
+
+  const [loginEmail, setLoginUser] = useState("");  //initialized the state of inputs for login
+  const [loginPassword, setLoginPassword] = useState("");
+  
+  async function handleLogin() { // Login with Google by creating an OAuth2 session
+    account.createOAuth2Session('google', 'http://localhost:5173', 'http://localhost:5173/fail')
+  }
+
+  async function handleCreateAccount() { // Create an account using username and password
+    const promise = account.create(name, email, password);
+    promise.then(function (response) {
+      console.log(response); // Success
+  }, function (error) {
+      console.log(error); // Failure
+  });
+  }
+
+  async function handleExistingAccount() { // Login with an existing account
+    const promise = account.createEmailPasswordSession(loginEmail, loginPassword);
+    promise.then(function (response) {
+      console.log(response); // Success
+  }, function (error) {
+      console.log(error); // Failure
+  });
+
+  }
+
+  function toggle() { // Toggle between Sign Up and Sign In screens 
+    const signUp = document.querySelector('.signUp');
+    const signIn = document.querySelector('.signIn');
+    const signUpBtn = document.querySelector('.signup-btn');
+    const signInBtn = document.querySelector('.singin-btn');
+
+    if (signUp.style.display === 'none') {
+      signUp.style.display = 'block';
+      signIn.style.display = 'none';
+      signUpBtn.style.display = 'none';
+      signInBtn.style.display = 'block';
+    } else {
+      signUp.style.display = 'none';
+      signIn.style.display = 'block';
+      signUpBtn.style.display = 'block';
+      signInBtn.style.display = 'none';
+    }
+    
+  }
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
+    <div className="loginPage">
 
+      <img src={background} alt='background' className='background'/> {/* Background image*/}
+      <div className='container'>
+        <div className='signUp' style={{display: 'none'}}>   {/* Sign up hidden by default*/}
+          <h1>Sign Up</h1>
+          <input type="text" placeholder="Full Name" onChange={e => setName(e.target.value)}/>   {/* Inputs for signup*/}
+          <input type="email" placeholder="Email" onChange={e => setUser(e.target.value)}/>           
+          <input type="password" placeholder="Password" onChange={e => setPassword(e.target.value)}/>
+          <button onClick={handleCreateAccount}>Create Account</button>   {/* Button to create account*/}
+
+          <h3>Or sign in with Google</h3>
+          <GoogleButton onClick={handleLogin}/>  {/* Button to sign in with Google*/}
+          <h3>Or Sign-In Using</h3>
+          <button className='singin-btn' onClick={toggle}>Sign-In</button>  {/* Button to toggle to sign in screen*/}
+        </div>
+      
+        <div className='signIn'>
+          <h1>Login</h1> 
+          <input type="email" placeholder="Type your email" onChange={e => setLoginUser(e.target.value)}/> {/* Inputs for sign in*/}
+          <input type="password" placeholder="Type your password" onChange={e => setLoginPassword(e.target.value)}/> 
+          <button onClick={handleExistingAccount}>Login</button>  {/* Button to login*/}
+
+          <h3>Or sign in with Google</h3>
+          <GoogleButton onClick={handleLogin}/> {/* Button to sign in with Google*/}
+
+          <h3>Or Sign-Up Using</h3>
+          <button className=' signup-btn' onClick={toggle}>Sign Up</button> {/* Button to toggle to sign up screen*/}
+        </div>
+      </div>
+    </div>
+  );
+}
 export default App
