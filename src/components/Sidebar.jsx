705a92a5d2e00b533e09184e16795d4aab5902e1
Ammar9faisal <134812365+Ammar9faisal@users.noreply.github.com>
2025-02-22 22:27:20 -0500

    added sidebar documentation, plus fixed global background error

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,44 +1,44 @@
 import React, { useState } from 'react';
-import { BarChart2, Heart, Users, Settings, LogOut } from 'lucide-react';
+import { BarChart2, Smile, Users, Settings, LogOut } from 'lucide-react';
 import './sidebar.css';
 
 export function Sidebar() {
-  const [activeButton, setActiveButton] = useState('dashboard');
+  const [activeButton, setActiveButton] = useState('dashboard');  // 'dashboard' is the default active button
 
-  const handleButtonClick = (buttonName) => {
+  const handleButtonClick = (buttonName) => {  // sets the button as active when a button is clicekd
     setActiveButton(buttonName);
   };
 
   return (
     <div className="sidebar">
-      <div className="sidebar-logo">LOGO</div>
+      <div className="sidebar-logo">Logo</div>
       <nav className="sidebar-nav">
         <button
           className={`sidebar-button ${activeButton === 'dashboard' ? 'sidebar-button-active' : ''}`}
-          onClick={() => handleButtonClick('dashboard')}
+          onClick={() => handleButtonClick('dashboard')}  // sets the button as active when a button is clicekd
         >
           <BarChart2 className="sidebar-icon" color="white"/>
         </button>
         <button
           className={`sidebar-button ${activeButton === 'heart' ? 'sidebar-button-active' : ''}`}
-          onClick={() => handleButtonClick('heart')}
+          onClick={() => handleButtonClick('heart')}  // sets the button as active when a button is clicekd
         >
-          <Heart className="sidebar-icon"  color="white"/>
+          <Smile className="sidebar-icon"  color="white"/>
         </button>
         <button
           className={`sidebar-button ${activeButton === 'users' ? 'sidebar-button-active' : ''}`}
-          onClick={() => handleButtonClick('users')}
+          onClick={() => handleButtonClick('users')} // sets the button as active when a button is clicekd
         >
           <Users className="sidebar-icon"  color="white"/>
         </button>
         <button
           className={`sidebar-button ${activeButton === 'settings' ? 'sidebar-button-active' : ''}`}
-          onClick={() => handleButtonClick('settings')}
+          onClick={() => handleButtonClick('settings')}   // sets the button as active when a button is clicekd
         >
           <Settings className="sidebar-icon"  color="white"/>
         </button>
       </nav>
-      <button className="sidebar-logout">
+      <button className="sidebar-button">
         <LogOut className="sidebar-icon"  color="white"/>
       </button>
     </div>
