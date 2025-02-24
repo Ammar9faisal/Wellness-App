2f3ae267696583824e3c0a31e6849e6525833518
Ammar9faisal <134812365+Ammar9faisal@users.noreply.github.com>
2025-02-24 01:20:24 -0500

    created example stub database

---
+++ src/stubdata.js
@@ -0,0 +1,19 @@
+// Combined stub data
+export const stubData = {
+    userProfile: {
+        username: 'John Doe',
+        email: 'johndoe@gmail.com',
+        password: 'password',
+        age: 30
+    },
+    wellnessIndexDaily: [
+        { day: 'Mon', wellnessIndex: 8 },
+        { day: 'Tues', wellnessIndex: 7 },
+        { day: 'Wed', wellnessIndex: 9 },
+        { day: 'Thur', wellnessIndex: 6 },
+        { day: 'Fri', wellnessIndex: 8 },
+        { day: 'Sat', wellnessIndex: 9 },
+        { day: 'Sun', wellnessIndex: 9 }
+    ]
+};
+
